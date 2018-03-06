96d05141d24a1f4f2f65593387089f7a215195ee
Leila Hadjioual <leila.hadjioual@Digital-Grenoble.local>
2018-03-06 17:07:42 +0100

    add map

--- src/MachinesMap.vue
+++ src/MachinesMap.vue
@@ -1,12 +1,20 @@
 <template>
+  <div>
     <h1>Carte des machines</h1>
+    <gmap-map
+      :center="{lat:45.1885, lng:5.7245}"
+      :zoom="12"
+      style="width: 100%; height: 800px">
+    </gmap-map>
+  </div>
+
 </template>
 
 <script>
-    export default {
-        name: "machinesMap",
-        // props : ['map']
-    }
+  export default {
+    name: "machinesMap",
+    // props : ['map']
+  }
 </script>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -3,17 +3,25 @@ import App from './App.vue'
 import list from './MachinesList.vue'// nom de la props from fichier où est définit la props
 Vue.component('ListMachines', list); // nom que l'on définit (à appeler dans la balise) , nom de la props
 import map from './MachinesMap.vue'
+
 Vue.component('MapMachines', map);
 import Machine from './Machine.vue'
-// Vue.component('name', Machine);
 import VueRouter from 'vue-router' //installer vue router : npm install vue-router
 Vue.use(VueRouter);
+import * as VueGoogleMaps from 'vue2-google-maps'//installer vue map : npm install vue2-google-maps
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: ' AIzaSyAFitITUROy2w4_WCjA92S0Axj-Hc2WCDA ',//clé API récupérée sur googlemap api
+    libraries: 'places',
+  }
+});
 
 //création des routes
 const routes = [
   {path: '/machines', component: list},
   {path: '/map', component: map},
-  {path:'/machine', component: Machine},
+  {path: '/machine', component: Machine},
 ];
 
 const router = new VueRouter({
